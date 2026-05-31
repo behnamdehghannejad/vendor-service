af12ae80c48ad73aa5032ac5c86fa89ff4628043
mohamadreza <mohamadrezamomen97@gmail.com>
2026-05-31 14:45:15 +0330

    refacto: discard dead code

--- internal/domain/inventory.go
+++ internal/domain/inventory.go
@@ -1,22 +1,9 @@
 package domain
 
-import (
-	"errors"
-)
-
 type Inventory struct {
 	ID        int
 	VendorID  int
 	ProductID int
 	Quantity  int
 	Reserved  int
-}
-
-func ValidateAndSetQuantity(inventory *Inventory, quantity int) error {
-	if quantity > inventory.Quantity {
-		return errors.New("inventory quantity is more than the order quantity")
-	}
-
-	inventory.Quantity -= quantity
-	return nil
 }
